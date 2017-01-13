1219c98105b89c6af6fff55f025691e162db62e7
Alvin Wan <hi@alvinwan.com>
2017-01-12 21:15:18 -0800

    added splash screen, nav

---
+++ iOSApp2017/NavigationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NavigationViewController.swift
+//  iOSApp2017
+//
+//  Created by Alvin Wan on 1/12/17.
+//  Copyright © 2017 Alvin Wan. All rights reserved.
+//
+
+import UIKit
+
+
+class NavigationViewController: UIViewController {
+    
+}
